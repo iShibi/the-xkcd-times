4100b3c2f14cbac15fd7dee1d164a7aada63ac77
Shubham Parihar <shubhamparihar391@gmail.com>
2022-11-12 18:25:39 +0530

    fix: missing swc deps in lockfile

--- src/app/[issue]/page.tsx
+++ src/app/[issue]/page.tsx
@@ -1,16 +1,17 @@
 import ComicPanel from '../ComicPanel';
 
 interface ArchivedIssueProps {
-	params: {
+	params?: {
 		issue: number;
 	};
+	searchParams?: unknown;
 }
 
 export default function ArchivedIssue({ params }: ArchivedIssueProps) {
 	return (
 		<>
 			<main className='min-h-screen'>
-				<ComicPanel issue={params.issue} />
+				<ComicPanel issue={params?.issue} />
 			</main>
 		</>
 	);
