2d11e0fac7ebb62cb0b3f74039e66bc145365d0d
Shubham Parihar <shubhamparihar391@gmail.com>
2022-11-12 18:45:04 +0530

    fix: type error for ArchivedIssue page props

--- src/app/[issue]/page.tsx
+++ src/app/[issue]/page.tsx
@@ -2,17 +2,14 @@ import ComicPanel from '../ComicPanel';
 
 interface ArchivedIssueProps {
 	params?: {
-		issue: number;
+		issue?: string;
 	};
-	searchParams?: unknown;
 }
 
 export default function ArchivedIssue({ params }: ArchivedIssueProps) {
 	return (
 		<>
-			<main className='min-h-screen'>
-				<ComicPanel issue={params?.issue} />
-			</main>
+			<main className='min-h-screen'>{params?.issue ? <ComicPanel issue={parseInt(params.issue)} /> : <></>}</main>
 		</>
 	);
 }
